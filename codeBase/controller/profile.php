3a3059f52c245f6d265dbfcb7cbca0bdc99f91a1
la3mmchen <pge@ideacloud.us>
2011-04-11 08:19:58 +0200

    tabula rasa

--- codeBase/controller/profile.php
+++
@@ -1,6 +0,0 @@
-<?php
-class profile extends MultiConstructor {
-
-
-}
-?>
--- codeBase/dbConfig.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$conf_dbHost = "localhost";
-	$conf_dbUser = "fdbck";
-	$conf_dbPw = "fdbck";
-	$conf_dbDatabase = "ideacloud";
-?>
